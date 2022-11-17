9868c606ca1ffa78cf34c98d571e92c083b78d1e
John Owen Danuarta <john.owen.d19@gmail.com>
2022-11-17 22:07:12 +0700

    Code: Adding login UI button navigation from LoginFragment to DashboardFragment.

--- app/src/main/java/com/john/kebunku/view/LoginFragment.kt
+++ app/src/main/java/com/john/kebunku/view/LoginFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.john.kebunku.R
+import kotlinx.android.synthetic.main.fragment_login.view.*
+
 class LoginFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -13,5 +16,13 @@ class LoginFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.buttonLogin.setOnClickListener {
+            val action = LoginFragmentDirections.actionDashboardFragment()
+            Navigation.findNavController(view).navigate(action)
+        }
     }
 }
