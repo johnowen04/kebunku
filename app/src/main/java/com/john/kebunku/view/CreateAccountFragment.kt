0de872f40115ce18c2e2ee6bdb0c70f611166502
John Owen Danuarta <john.owen.d19@gmail.com>
2022-11-18 22:15:52 +0700

    Code: Adding button register user logic UI in CreateAccountFragment.

--- app/src/main/java/com/john/kebunku/view/CreateAccountFragment.kt
+++ app/src/main/java/com/john/kebunku/view/CreateAccountFragment.kt
@@ -5,8 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
 import com.john.kebunku.R
+import com.john.kebunku.viewmodel.CreateAccountViewModel
+import kotlinx.android.synthetic.main.fragment_create_account.*
+import kotlinx.android.synthetic.main.fragment_edit_account.*
+
 class CreateAccountFragment : Fragment() {
+    private lateinit var viewModel: CreateAccountViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -17,5 +25,19 @@ class CreateAccountFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this).get(CreateAccountViewModel::class.java)
+
+        buttonRegisterUser.setOnClickListener {
+            if (editPasswordCreateFragment.text.toString() == editRetypePasswordCreateFragment.toString()) {
+                viewModel.register(
+                    editUsernameCreateFragment.text.toString(),
+                    editNameCreateFragment.text.toString(),
+                    editPasswordCreateFragment.text.toString()
+                )
+
+                Navigation.findNavController(it).popBackStack()
+            }
+        }
     }
 }
