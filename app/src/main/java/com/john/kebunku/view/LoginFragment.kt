95279a4e94425cbee8cbc8c4ea3cf97cd3518e6b
John Owen Danuarta <john.owen.d19@gmail.com>
2022-11-18 23:45:51 +0700

    Code: Adding login logic in LoginFragment using ViewModel.

--- app/src/main/java/com/john/kebunku/view/LoginFragment.kt
+++ app/src/main/java/com/john/kebunku/view/LoginFragment.kt
@@ -5,11 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
 import com.john.kebunku.R
+import com.john.kebunku.viewmodel.LoginViewModel
 import kotlinx.android.synthetic.main.fragment_login.*
 
 class LoginFragment : Fragment() {
+    private lateinit var viewModel: LoginViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -20,9 +25,33 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
+
         buttonLogin.setOnClickListener {
-            val action = LoginFragmentDirections.actionDashboardFragment()
-            Navigation.findNavController(view).navigate(action)
+            var login = true
+
+            if (editUsername.text.toString().isEmpty()) {
+                login = false
+                Toast.makeText(view.context, "Username cannot be empty", Toast.LENGTH_SHORT).show()
+            } else if (editPassword.text.toString().isEmpty()) {
+                login = false
+                Toast.makeText(view.context, "Password cannot be empty", Toast.LENGTH_SHORT).show()
+            }
+
+            if (login) {
+                viewModel.login(editUsername.text.toString(), editPassword.text.toString())
+
+                viewModel.userLiveData.observe(viewLifecycleOwner) {
+                    if (it != null) {
+                        Toast.makeText(view.context, "Login successful", Toast.LENGTH_SHORT).show()
+                        val action = LoginFragmentDirections.actionDashboardFragment()
+                        Navigation.findNavController(view).navigate(action)
+                    } else {
+                        Toast.makeText(view.context, "Login failed", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
         }
 
         buttonRegister.setOnClickListener {
