6290505d8b86c76528ca552e2c54e335b43ad08a
John Owen Danuarta <john.owen.d19@gmail.com>
2022-11-18 20:49:22 +0700

    Code: Adding user name to User data class.

--- app/src/main/java/com/john/kebunku/model/user/User.kt
+++ app/src/main/java/com/john/kebunku/model/user/User.kt
@@ -8,6 +8,8 @@ import androidx.room.PrimaryKey
 data class User(
     @ColumnInfo(name = "username")
     var username: String,
+    @ColumnInfo(name = "name")
+    var name: String,
     @ColumnInfo(name = "password")
     var password: String
 ) {
